0c2f1872dceb09b73143dfc2fb921c8b4148e8d1
John Paul Davis <john@johnpauldavis.org>
2022-04-26 14:33:56 -0400

    feat9RichText): basic component setup

---
+++ packages/react/src/stories/RichText.stories.tsx
@@ -0,0 +1,57 @@
+import { Story, Meta } from '@storybook/react';
+import { Title, Subtitle, Description, Primary, ArgsTable, Stories} from '@storybook/addon-docs';
+import { RichText }  from '../components/RichText';
+import { RichTextProps } from '../components/RichText/RichText.props';
+import richTextArgs from '../components/RichText/richText.args';
+
+/**
+ * RichText Story
+ *
+ */
+export default {
+  title: 'Components/RichText',
+  component: RichText,
+  argTypes: {},
+  parameters: {
+    componentSubtitle: 'Component',
+    docs: {
+      page: () => (
+        <>
+          <Subtitle />
+          <Title />
+          <Description>
+          The rich text component displays HTML as composed in a WYSIWYG editor.
+          </Description>
+          <Primary />
+          <ArgsTable />
+          <Stories title="Examples">
+          </Stories>
+        </>
+      ),
+    },
+  },
+} as Meta<typeof RichText>;
+
+/**
+ * RichText Template
+ *
+ * create a Storybook template for this component
+ *
+ *@param (Object) args - props to be passed to the component
+ */
+const RichTextTemplate: Story<RichTextProps> = (args) => (
+  <RichText {...args} />
+);
+
+
+/**
+ * RichText Instance
+ *
+ */
+ export const BaseRichText = RichTextTemplate.bind({});
+
+
+// enumerate the props for the rich text component
+BaseRichText.args = richTextArgs.richtext;
+console.log(BaseRichText.args);
+BaseRichText.storyName = 'Rich Text';
