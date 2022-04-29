d32fe4e78656b76bdb6b58a58161b096bc9cef10
John Paul Davis <john@johnpauldavis.org>
2022-04-29 09:22:46 -0400

    feat9ReadMore): intital scaffolding

---
+++ packages/react/src/components/ReadMore/ReadMore.props.d.ts
@@ -0,0 +1,59 @@
+import { ReactNode, MouseEvent } from "react";
+import { buttonTypes } from "../../types";
+
+export interface ButtonProps {
+  /**
+   * Specify the callback of your Button.
+   */
+  callback: function;
+
+  /**
+   * Specify the content of your Button.
+   */
+  children: ReactNode;
+
+  /**
+   * Specify an optional className to be added to your Button.
+   */
+  className?: string;
+
+  /**
+   * Specify whether or not the button is disabled.
+   */
+  disabled?: boolean;
+
+  /**
+   * Specify the icon for the Button
+   */
+  icon?: string;
+
+  /**
+   * Specify the icon for the Button
+   */
+  iconPosition?: positionTypes;
+
+  /**
+   * Specify the label for the Button
+   */
+  label?: string;
+
+  /**
+   * Specify an optional className to be added to your Button.
+   */
+  size?: sizeTypes;
+
+  /**
+   * Specify the target for when Button is really a link
+   */
+  target?: string;
+
+  /**
+   * Specify an optional className to be added to your Button.
+   */
+  type?: buttonTypes;
+
+  /**
+   * Specify the url for the Button's href
+   */
+  url?: string;
+}
