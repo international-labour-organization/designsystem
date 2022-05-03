c31d4caf82f4b59a5657a5884c891effa3310781
John Moore <jmoore@bluestate.co>
2022-05-03 11:59:50 -0700

    feat(List): Test tweaks
    Working on getting tests validated

--- packages/react/src/__tests__/List.test.tsx
+++ packages/react/src/__tests__/List.test.tsx
@@ -6,26 +6,26 @@ import * as stories from "../stories/List.stories";
 
 describe("<List>", () => {
   it("Should render `ol` with some items.", () => {
-    const listOrdered = render(<List {...listArgs.ordered} />);
-    expect(listOrdered).not.toBeNull();
-    expect(listOrdered).toHaveClass("ilo--list--ordered");
+    const { container } = render(<List {...listArgs.ordered} />);
+    expect(container).not.toBeNull();
+    expect(container.firstChild).toHaveClass("ilo--list--ordered");
   });
 
   it("Should render `ul` with some items.", () => {
-    const listUnordered = render(<List {...listArgs.unordered} />);
-    expect(listUnordered).not.toBeNull();
-    expect(listUnordered).toHaveClass("ilo--list--unordered");
+    const { container } = render(<List {...listArgs.unordered} />);
+    expect(container).not.toBeNull();
+    expect(container.firstChild).toHaveClass("ilo--list--unordered");
   });
 
   it("Should render `ul` horizontally with some items.", () => {
-    const listHorizontal = render(<List {...listArgs.horizontal} />);
-    expect(listHorizontal).not.toBeNull();
-    expect(listHorizontal).toHaveClass("ilo--list--horizontal");
+    const { container } = render(<List {...listArgs.horizontal} />);
+    expect(container).not.toBeNull();
+    expect(container.firstChild).toHaveClass("ilo--list--horizontal");
   });
 
   it("Should render `ul` unstyled with some items.", () => {
-    const listUnstyled = render(<List {...listArgs.unstyled} />);
-    expect(listUnstyled).not.toBeNull();
-    expect(listUnstyled).toHaveClass("ilo--list--unstyled");
+    const { container } = render(<List {...listArgs.unstyled} />);
+    expect(container).not.toBeNull();
+    expect(container.firstChild).toHaveClass("ilo--list--unstyled");
   });
 });
