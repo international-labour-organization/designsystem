a2b26288e2270bb775e147a981e157a94e75cc4f
John Paul Davis <john@johnpauldavis.org>
2022-05-04 15:12:53 -0400

    bugfix(List): don't hardcode test st rings that can be changed with the theme

--- packages/react/src/__tests__/List.test.tsx
+++ packages/react/src/__tests__/List.test.tsx
@@ -8,24 +8,32 @@ describe("<List>", () => {
   it("Should render `ol` with some items.", () => {
     const { container } = render(<List {...listArgs.ordered} />);
     expect(container).not.toBeNull();
-    expect(container.firstChild).toHaveClass("ilo--list--ordered");
+    expect(container.firstChild.getAttribute("class")).toEqual(
+      expect.stringContaining(listArgs.ordered.ordered)
+    );
   });
 
   it("Should render `ul` with some items.", () => {
     const { container } = render(<List {...listArgs.unordered} />);
     expect(container).not.toBeNull();
-    expect(container.firstChild).toHaveClass("ilo--list--unordered");
+    expect(container.firstChild.getAttribute("class")).toEqual(
+      expect.stringContaining(listArgs.unordered.ordered)
+    );
   });
 
   it("Should render `ul` horizontally with some items.", () => {
     const { container } = render(<List {...listArgs.horizontal} />);
     expect(container).not.toBeNull();
-    expect(container.firstChild).toHaveClass("ilo--list--horizontal");
+    expect(container.firstChild.getAttribute("class")).toEqual(
+      expect.stringContaining(listArgs.horizontal.alignment)
+    );
   });
 
   it("Should render `ul` unstyled with some items.", () => {
     const { container } = render(<List {...listArgs.unstyled} />);
     expect(container).not.toBeNull();
-    expect(container.firstChild).toHaveClass("ilo--list--unstyled");
+    expect(container.firstChild.getAttribute("class")).toEqual(
+      expect.stringContaining(listArgs.unstyled.ordered)
+    );
   });
 });
