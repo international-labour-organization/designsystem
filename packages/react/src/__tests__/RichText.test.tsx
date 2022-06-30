25ab9a71feb2ccd7668f0f099ff97d051a9bec7c
justintemps <jesmith627@gmail.com>
2022-06-30 16:21:55 +0200

    fix(react): failing tests

--- packages/react/src/__tests__/RichText.test.tsx
+++ packages/react/src/__tests__/RichText.test.tsx
@@ -5,9 +5,12 @@ import richTextArgs from "../components/RichText/richText.args";
 describe("<RichText>", () => {
   it("Should render `div` with content.", () => {
     const { container } = render(<RichText {...richTextArgs.richtext} />);
+    // Parse the content as a domstring and get the text content
+    const textContent = new DOMParser().parseFromString(
+      richTextArgs.richtext.content,
+      "text/html"
+    ).documentElement.textContent;
     expect(container.children[0]).not.toBeNull();
-    expect(container.children[0].textContent).toEqual(
-      richTextArgs.richtext.content
-    );
+    expect(container.children[0].textContent).toEqual(textContent);
   });
 });
