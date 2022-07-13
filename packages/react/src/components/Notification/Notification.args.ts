73134a280b594c7effb8a851fa9809c3823d6284
John Paul Davis <john@johnpauldavis.org>
2022-07-13 16:46:45 -0400

    feat(Notification): add icons and more CSS/theming

--- packages/react/src/components/Notification/Notification.args.ts
+++ packages/react/src/components/Notification/Notification.args.ts
@@ -3,12 +3,12 @@ import { NotificationProps } from "./Notification.props";
 const hascta: NotificationProps = {
   className: "storybook",
   closelabel: "Close",
-  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   cta: {
     label: "Optional CTA",
     url: "http://www.google.com",
   },
-  headline: "Notification With CTA",
+  headline: "With CTA",
   placement: "inline",
   type: "info",
   timestamp: {
@@ -20,7 +20,7 @@ const hascta: NotificationProps = {
 const nocta: NotificationProps = {
   className: "storybook",
   closelabel: "Close",
-  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   headline: "Notification Info",
   placement: "inline",
   type: "info",
@@ -33,7 +33,7 @@ const nocta: NotificationProps = {
 const error: NotificationProps = {
   className: "storybook",
   closelabel: "Close",
-  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   headline: "Notification Error",
   placement: "inline",
   type: "error",
@@ -46,7 +46,7 @@ const error: NotificationProps = {
 const success: NotificationProps = {
   className: "storybook",
   closelabel: "Close",
-  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   headline: "Notification Success",
   placement: "inline",
   type: "success",
@@ -59,7 +59,7 @@ const success: NotificationProps = {
 const warning: NotificationProps = {
   className: "storybook",
   closelabel: "Close",
-  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   headline: "Notification Warning",
   placement: "inline",
   type: "warning",
@@ -72,7 +72,7 @@ const warning: NotificationProps = {
 const hasctadialog: NotificationProps = {
   className: "storybook",
   closelabel: "Close",
-  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   cta: {
     label: "Optional CTA",
     url: "http://www.google.com",
@@ -89,7 +89,7 @@ const hasctadialog: NotificationProps = {
 const infodialog: NotificationProps = {
   className: "storybook",
   closelabel: "Close",
-  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   headline: "Notification Info",
   placement: "dialog",
   type: "info",
