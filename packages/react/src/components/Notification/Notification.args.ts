9bd60b72ca81496bc16cbe6a27d41c3fd2d0703e
John Paul Davis <john@johnpauldavis.org>
2022-07-13 12:48:24 -0400

    feat(NOtification): React portion

---
+++ packages/react/src/components/Notification/Notification.args.ts
@@ -0,0 +1,157 @@
+import { NotificationProps } from "./Notification.props";
+
+const hascta: NotificationProps = {
+  className: "storybook",
+  closelabel: "Close",
+  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  cta: {
+    label: "Optional CTA",
+    url: "http://www.google.com",
+  },
+  headline: "Notification With CTA",
+  placement: "inline",
+  type: "info",
+  timestamp: {
+    human: "Dec 7, 2022",
+    unix: "1670389200",
+  },
+};
+
+const nocta: NotificationProps = {
+  className: "storybook",
+  closelabel: "Close",
+  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  headline: "Notification Info",
+  placement: "inline",
+  type: "info",
+  timestamp: {
+    human: "Dec 7, 2022",
+    unix: "1670389200",
+  },
+};
+
+const error: NotificationProps = {
+  className: "storybook",
+  closelabel: "Close",
+  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  headline: "Notification Error",
+  placement: "inline",
+  type: "error",
+  timestamp: {
+    human: "Dec 7, 2022",
+    unix: "1670389200",
+  },
+};
+
+const success: NotificationProps = {
+  className: "storybook",
+  closelabel: "Close",
+  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  headline: "Notification Success",
+  placement: "inline",
+  type: "success",
+  timestamp: {
+    human: "Dec 7, 2022",
+    unix: "1670389200",
+  },
+};
+
+const warning: NotificationProps = {
+  className: "storybook",
+  closelabel: "Close",
+  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  headline: "Notification Warning",
+  placement: "inline",
+  type: "warning",
+  timestamp: {
+    human: "Dec 7, 2022",
+    unix: "1670389200",
+  },
+};
+
+const hasctadialog: NotificationProps = {
+  className: "storybook",
+  closelabel: "Close",
+  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  cta: {
+    label: "Optional CTA",
+    url: "http://www.google.com",
+  },
+  headline: "Notification With CTA",
+  placement: "dialog",
+  type: "info",
+  timestamp: {
+    human: "Dec 7, 2022",
+    unix: "1670389200",
+  },
+};
+
+const infodialog: NotificationProps = {
+  className: "storybook",
+  closelabel: "Close",
+  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  headline: "Notification Info",
+  placement: "dialog",
+  type: "info",
+  timestamp: {
+    human: "Dec 7, 2022",
+    unix: "1670389200",
+  },
+};
+
+const errordialog: NotificationProps = {
+  className: "storybook",
+  closelabel: "Close",
+  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  headline: "Notification Error",
+  placement: "dialog",
+  type: "error",
+  timestamp: {
+    human: "Dec 7, 2022",
+    unix: "1670389200",
+  },
+};
+
+const successdialog: NotificationProps = {
+  className: "storybook",
+  closelabel: "Close",
+  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  headline: "Notification Success",
+  placement: "dialog",
+  type: "success",
+  timestamp: {
+    human: "Dec 7, 2022",
+    unix: "1670389200",
+  },
+};
+
+const warningdialog: NotificationProps = {
+  className: "storybook",
+  closelabel: "Close",
+  copy: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+  headline: "Notification Warning",
+  placement: "dialog",
+  type: "warning",
+  timestamp: {
+    human: "Dec 7, 2022",
+    unix: "1670389200",
+  },
+};
+
+/**
+ * Sample prop definitions for Notification's enumerable properties (imported in stories and test)
+ */
+const NotificationArgs = {
+  error,
+  errordialog,
+  hascta,
+  hasctadialog,
+  infodialog,
+  nocta,
+  success,
+  successdialog,
+  warning,
+  warningdialog,
+};
+
+export default NotificationArgs;
---
+++ packages/react/src/components/Notification/Notification.tsx
@@ -0,0 +1,78 @@
+import { FC, useState } from "react";
+import classNames from "classnames";
+import useGlobalSettings from "../../hooks/useGlobalSettings";
+import { NotificationProps } from "./Notification.props";
+import { ButtonProps } from "../Button/Button.props";
+import { Button } from "../Button";
+
+const Notification: FC<NotificationProps> = ({
+  className,
+  closelabel,
+  copy,
+  cta,
+  headline,
+  placement,
+  timestamp,
+  type,
+}) => {
+  const { prefix } = useGlobalSettings();
+
+  const [display, setDisplay] = useState(true);
+
+  const baseClass = `${prefix}--notification`;
+  const notificationClasses = classNames(className, {
+    [baseClass]: true,
+    [`${baseClass}--${placement}`]: placement,
+    [`icon icon--${type}`]: type,
+  });
+
+  const ctaprops: ButtonProps = {
+    callback: false,
+    className: `${baseClass}--cta`,
+    children: false,
+    label: cta?.label,
+    size: "small",
+    type: "secondary",
+    url: cta?.url,
+  };
+
+  /**
+   * On click, close
+   */
+  const handleClick = () => {
+    setDisplay(false);
+  };
+
+  return (
+    <>
+      {display && (
+        <div
+          className={notificationClasses}
+          role="status"
+          aria-live="polite"
+          aria-relevant="additions"
+        >
+          <div className={`${baseClass}--content`}>
+            <h2 className={`${baseClass}--headline`}>{headline}</h2>
+            <p className={`${baseClass}--copy`}>{copy}</p>
+            {timestamp && (
+              <time className={`${baseClass}--date`} dateTime={timestamp.unix}>
+                {timestamp.human}
+              </time>
+            )}
+            {cta && <Button {...ctaprops} />}
+          </div>
+          <button
+            className={`${baseClass}--close`}
+            type="button"
+            onClick={() => handleClick()}
+          >
+            {closelabel}
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Notification;
