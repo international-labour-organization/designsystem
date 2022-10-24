b4083e8829554d889e59bea084302c6ef70cd8e7
John Moore <jmoore@bluestate.co>
2022-10-23 20:51:59 -0700

    feat(Navigation): wingsuit connection and search field
    Add in the wingsuit bits and include the search field component

--- packages/twig/src/patterns/components/navigation/navigation.js
+++ packages/twig/src/patterns/components/navigation/navigation.js
@@ -50,7 +50,11 @@ export default class Navigation {
      * The button for toggling Subnav state
      * @type {Object}
      */
-    this.SubnavButton = this.element.querySelector(`.${this.prefix}--nav--trigger`);
+    this.subnavButton = this.element.querySelector(`.${this.prefix}--nav--trigger`);
+    this.menuCloseSet = this.element.querySelectorAll(`.${this.prefix}--header--menu--close`);
+    this.subnavBack = this.element.querySelector(`.${this.prefix}--mobile--subnav--back`);
+    this.menuOpen = this.element.querySelector(`.${this.prefix}--header--menu`);
+    this.searchButton = this.element.querySelector(`.${this.prefix}--search--button`);
 
     return this;
   }
@@ -62,7 +66,11 @@ export default class Navigation {
    * @chainable
    */
   setupHandlers() {
-    this.subnavClick = this.handleSubnavClick.bind(this);
+    this.subnavOpenClick = this.handleSubnavClickOpen.bind(this);
+    this.subnavBackClick = this.handleSubnavBackClick.bind(this);
+    this.menuCloseClick = this.handleMenuCloseClick.bind(this);
+    this.menuOpenClick = this.handleMenuOpenClick.bind(this);
+    this.searchButtonClick = this.handleSearchButtonClick.bind(this);
 
     return this;
   }
@@ -74,7 +82,57 @@ export default class Navigation {
    * @chainable
    */
   enable() {
-    this.SubnavButton.addEventListener(EVENTS.CLICK, () => this.subnavClick());
+    // subnavButton
+    if (this.subnavButton) {
+      this.subnavButton.addEventListener(EVENTS.CLICK, () => this.subnavOpenClick());
+      this.subnavButton.addEventListener(EVENTS.TOUCH_START, () => this.subnavOpenClick());
+    }
+    // subnavBack
+    if (this.subnavBack) {
+      this.subnavBack.addEventListener(EVENTS.CLICK, () => this.subnavBackClick());
+      this.subnavBack.addEventListener(EVENTS.TOUCH_START, () => this.subnavBackClick());
+    }
+    // searchButton
+    if (this.searchButton) {
+      this.searchButton.addEventListener(EVENTS.CLICK, () => this.searchButtonClick());
+      this.searchButton.addEventListener(EVENTS.TOUCH_START, () => this.searchButtonClick());
+    }
+    // menuCloseSet
+    if (this.menuCloseSet.length > 0) {
+      this.menuCloseSet.forEach((closeButton, i) => {
+        closeButton.addEventListener(EVENTS.CLICK, () => this.menuCloseClick(i));
+        closeButton.addEventListener(EVENTS.TOUCH_START, () => this.menuCloseClick(i));
+      });
+    }
+
+    // menuOpen
+    this.menuOpen.addEventListener(EVENTS.CLICK, () => this.menuOpenClick());
+    this.menuOpen.addEventListener(EVENTS.TOUCH_START, () => this.menuOpenClick());
+
+    return this;
+  }
+
+  /**
+   * Onclick interaction with the search button
+   *
+   * @return {Object} Navigation A reference to the instance of the class
+   * @chainable
+   */
+  handleSearchButtonClick() {
+    this.element.classList.toggle(`${this.prefix}--search--open`);
+    this.element.classList.remove(`${this.prefix}--subnav--open`);
+
+    return this;
+  }
+
+  /**
+   * Onclick interaction with the menu button
+   *
+   * @return {Object} Navigation A reference to the instance of the class
+   * @chainable
+   */
+  handleMenuOpenClick() {
+    this.element.classList.add(`${this.prefix}--mobile--open`);
 
     return this;
   }
@@ -85,8 +143,34 @@ export default class Navigation {
    * @return {Object} Navigation A reference to the instance of the class
    * @chainable
    */
-  handleSubnavClick() {
+  handleSubnavClickOpen() {
     this.element.classList.toggle(`${this.prefix}--subnav--open`);
+    this.element.classList.remove(`${this.prefix}--search--open`);
+
+    return this;
+  }
+
+  /**
+   * Onclick interaction with the Subnav button
+   *
+   * @return {Object} Navigation A reference to the instance of the class
+   * @chainable
+   */
+  handleSubnavBackClick() {
+    this.element.classList.remove(`${this.prefix}--subnav--open`);
+
+    return this;
+  }
+
+  /**
+   * Onclick interaction with the menu close button
+   *
+   * @return {Object} Navigation A reference to the instance of the class
+   * @chainable
+   */
+  handleMenuCloseClick() {
+    this.element.classList.remove(`${this.prefix}--mobile--open`);
+    this.element.classList.remove(`${this.prefix}--subnav--open`);
 
     return this;
   }
