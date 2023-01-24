e6d0204d46bc1dd02f01c646e9db22f20b639ccd
justintemps <jesmith627@gmail.com>
2023-01-24 16:00:17 +0100

    feat(brand-assets): add en/fr/es logos

--- packages/brand-assets/src/index.js
+++ packages/brand-assets/src/index.js
@@ -1,4 +1,84 @@
+const logo_en_horizontal_blue_png = new URL(
+  "./assets/logo_en_horizontal_blue.png",
+  import.meta.url
+);
+
+const logo_en_horizontal_blue_svg = new URL(
+  "./assets/logo_en_horizontal_blue.svg",
+  import.meta.url
+);
+
+const logo_en_horizontal_white_png = new URL(
+  "./assets/logo_en_horizontal_white.png",
+  import.meta.url
+);
+
+const logo_en_horizontal_white_svg = new URL(
+  "./assets/logo_en_horizontal_white.svg",
+  import.meta.url
+);
+
+const logo_es_horizontal_blue_png = new URL(
+  "./assets/logo_es_horizontal_blue.png",
+  import.meta.url
+);
+
+const logo_es_horizontal_blue_svg = new URL(
+  "./assets/logo_es_horizontal_blue.svg",
+  import.meta.url
+);
+
+const logo_es_horizontal_white_png = new URL(
+  "./assets/logo_es_horizontal_white.png",
+  import.meta.url
+);
+
+const logo_es_horizontal_white_svg = new URL(
+  "./assets/logo_es_horizontal_white.svg",
+  import.meta.url
+);
+
+const logo_fr_horizontal_blue_png = new URL(
+  "./assets/logo_fr_horizontal_blue.png",
+  import.meta.url
+);
+
+const logo_fr_horizontal_blue_svg = new URL(
+  "./assets/logo_fr_horizontal_blue.svg",
+  import.meta.url
+);
+
+const logo_fr_horizontal_white_png = new URL(
+  "./assets/logo_fr_horizontal_white.png",
+  import.meta.url
+);
+
+const logo_fr_horizontal_white_svg = new URL(
+  "./assets/logo_fr_horizontal_white.svg",
+  import.meta.url
+);
+
+const favicon = new URL("./assets/favicon.ico", import.meta.url);
+
+/**
+ * LEGACY, these will be removed in future versions
+ */
 const logo = new URL("./assets/ilo.png", import.meta.url);
 const logofr = new URL("./assets/ilo-fr.png", import.meta.url);
 
-export { logo, logofr };
+export {
+  logo,
+  logofr,
+  logo_en_horizontal_blue_png,
+  logo_en_horizontal_blue_svg,
+  logo_en_horizontal_white_png,
+  logo_en_horizontal_white_svg,
+  logo_es_horizontal_blue_png,
+  logo_es_horizontal_blue_svg,
+  logo_es_horizontal_white_png,
+  logo_es_horizontal_white_svg,
+  logo_fr_horizontal_blue_png,
+  logo_fr_horizontal_blue_svg,
+  logo_fr_horizontal_white_png,
+  logo_fr_horizontal_white_svg,
+};
