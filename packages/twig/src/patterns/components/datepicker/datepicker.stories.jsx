28b0b73e1e2894b135affeadb7a0137ba73c1923
justintemps <jesmith627@gmail.com>
2023-09-06 16:10:51 +0200

    refactor(twig): datepicker

---
+++ packages/twig/src/patterns/components/datepicker/datepicker.stories.jsx
@@ -0,0 +1,9 @@
+import "./index";
+
+export default {};
+const patternDefinition = require("./datepicker.wingsuit.yml");
+
+/* eslint-disable-next-line */
+export const wingsuit = {
+  patternDefinition,
+};
