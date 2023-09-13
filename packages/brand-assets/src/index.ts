c0fb36a66505c25e25a0e295c53d17bcbd877732
Weichen LEI <weichen.lei@outlook.com>
2023-09-13 14:03:32 +0200

    feat(brand-assets): add additional logos in some languages

--- packages/brand-assets/src/index.ts
+++ packages/brand-assets/src/index.ts
@@ -58,6 +58,206 @@ const logo_fr_horizontal_white_svg = new URL(
   import.meta.url
 );
 
+const logo_ar_horizontal_blue_png = new URL(
+  "./assets/logo_ar_horizontal_blue.png",
+  import.meta.url
+);
+
+const logo_ar_horizontal_blue_svg = new URL(
+  "./assets/logo_ar_horizontal_blue.svg",
+  import.meta.url
+);
+
+const logo_ar_horizontal_white_png = new URL(
+  "./assets/logo_ar_horizontal_white.png",
+  import.meta.url
+);
+
+const logo_ar_horizontal_white_svg = new URL(
+  "./assets/logo_ar_horizontal_white.svg",
+  import.meta.url
+);
+
+const logo_de_horizontal_blue_png = new URL(
+  "./assets/logo_de_horizontal_blue.png",
+  import.meta.url
+);
+
+const logo_de_horizontal_blue_svg = new URL(
+  "./assets/logo_de_horizontal_blue.svg",
+  import.meta.url
+);
+
+const logo_de_horizontal_white_png = new URL(
+  "./assets/logo_de_horizontal_white.png",
+  import.meta.url
+);
+
+const logo_de_horizontal_white_svg = new URL(
+  "./assets/logo_de_horizontal_white.svg",
+  import.meta.url
+);
+
+const logo_it_horizontal_blue_png = new URL(
+  "./assets/logo_it_horizontal_blue.png",
+  import.meta.url
+);
+
+const logo_it_horizontal_blue_svg = new URL(
+  "./assets/logo_it_horizontal_blue.svg",
+  import.meta.url
+);
+
+const logo_it_horizontal_white_png = new URL(
+  "./assets/logo_it_horizontal_white.png",
+  import.meta.url
+);
+
+const logo_it_horizontal_white_svg = new URL(
+  "./assets/logo_it_horizontal_white.svg",
+  import.meta.url
+);
+
+const logo_ja_horizontal_blue_png = new URL(
+  "./assets/logo_ja_horizontal_blue.png",
+  import.meta.url
+);
+
+const logo_ja_horizontal_blue_svg = new URL(
+  "./assets/logo_ja_horizontal_blue.svg",
+  import.meta.url
+);
+
+const logo_ja_horizontal_white_png = new URL(
+  "./assets/logo_ja_horizontal_white.png",
+  import.meta.url
+);
+
+const logo_ja_horizontal_white_svg = new URL(
+  "./assets/logo_ja_horizontal_white.svg",
+  import.meta.url
+);
+
+const logo_nl_horizontal_blue_png = new URL(
+  "./assets/logo_nl_horizontal_blue.png",
+  import.meta.url
+);
+
+const logo_nl_horizontal_blue_svg = new URL(
+  "./assets/logo_nl_horizontal_blue.svg",
+  import.meta.url
+);
+
+const logo_nl_horizontal_white_png = new URL(
+  "./assets/logo_nl_horizontal_white.png",
+  import.meta.url
+);
+
+const logo_nl_horizontal_white_svg = new URL(
+  "./assets/logo_nl_horizontal_white.svg",
+  import.meta.url
+);
+
+const logo_pt_horizontal_blue_png = new URL(
+  "./assets/logo_pt_horizontal_blue.png",
+  import.meta.url
+);
+
+const logo_pt_horizontal_blue_svg = new URL(
+  "./assets/logo_pt_horizontal_blue.svg",
+  import.meta.url
+);
+
+const logo_pt_horizontal_white_png = new URL(
+  "./assets/logo_pt_horizontal_white.png",
+  import.meta.url
+);
+
+const logo_pt_horizontal_white_svg = new URL(
+  "./assets/logo_pt_horizontal_white.svg",
+  import.meta.url
+);
+
+const logo_ru_horizontal_blue_png = new URL(
+  "./assets/logo_ru_horizontal_blue.png",
+  import.meta.url
+);
+
+const logo_ru_horizontal_blue_svg = new URL(
+  "./assets/logo_ru_horizontal_blue.svg",
+  import.meta.url
+);
+
+const logo_ru_horizontal_white_png = new URL(
+  "./assets/logo_ru_horizontal_white.png",
+  import.meta.url
+);
+
+const logo_ru_horizontal_white_svg = new URL(
+  "./assets/logo_ru_horizontal_white.svg",
+  import.meta.url
+);
+
+const logo_tr_horizontal_blue_png = new URL(
+  "./assets/logo_tr_horizontal_blue.png",
+  import.meta.url
+);
+
+const logo_tr_horizontal_blue_svg = new URL(
+  "./assets/logo_tr_horizontal_blue.svg",
+  import.meta.url
+);
+
+const logo_tr_horizontal_white_png = new URL(
+  "./assets/logo_tr_horizontal_white.png",
+  import.meta.url
+);
+
+const logo_tr_horizontal_white_svg = new URL(
+  "./assets/logo_tr_horizontal_white.svg",
+  import.meta.url
+);
+
+const logo_vi_horizontal_blue_png = new URL(
+  "./assets/logo_vi_horizontal_blue.png",
+  import.meta.url
+);
+
+const logo_vi_horizontal_blue_svg = new URL(
+  "./assets/logo_vi_horizontal_blue.svg",
+  import.meta.url
+);
+
+const logo_vi_horizontal_white_png = new URL(
+  "./assets/logo_vi_horizontal_white.png",
+  import.meta.url
+);
+
+const logo_vi_horizontal_white_svg = new URL(
+  "./assets/logo_vi_horizontal_white.svg",
+  import.meta.url
+);
+
+const logo_zh_horizontal_blue_png = new URL(
+  "./assets/logo_zh_horizontal_blue.png",
+  import.meta.url
+);
+
+const logo_zh_horizontal_blue_svg = new URL(
+  "./assets/logo_zh_horizontal_blue.svg",
+  import.meta.url
+);
+
+const logo_zh_horizontal_white_png = new URL(
+  "./assets/logo_zh_horizontal_white.png",
+  import.meta.url
+);
+
+const logo_zh_horizontal_white_svg = new URL(
+  "./assets/logo_zh_horizontal_white.svg",
+  import.meta.url
+);
+
 const favicon = new URL("./assets/favicon.ico", import.meta.url);
 
 /**
@@ -82,4 +282,44 @@ export {
   logo_fr_horizontal_blue_svg,
   logo_fr_horizontal_white_png,
   logo_fr_horizontal_white_svg,
+  logo_ar_horizontal_blue_png,
+  logo_ar_horizontal_blue_svg,
+  logo_ar_horizontal_white_png,
+  logo_ar_horizontal_white_svg,
+  logo_de_horizontal_blue_png,
+  logo_de_horizontal_blue_svg,
+  logo_de_horizontal_white_png,
+  logo_de_horizontal_white_svg,
+  logo_it_horizontal_blue_png,
+  logo_it_horizontal_blue_svg,
+  logo_it_horizontal_white_png,
+  logo_it_horizontal_white_svg,
+  logo_ja_horizontal_blue_png,
+  logo_ja_horizontal_blue_svg,
+  logo_ja_horizontal_white_png,
+  logo_ja_horizontal_white_svg,
+  logo_nl_horizontal_blue_png,
+  logo_nl_horizontal_blue_svg,
+  logo_nl_horizontal_white_png,
+  logo_nl_horizontal_white_svg,
+  logo_pt_horizontal_blue_png,
+  logo_pt_horizontal_blue_svg,
+  logo_pt_horizontal_white_png,
+  logo_pt_horizontal_white_svg,
+  logo_ru_horizontal_blue_png,
+  logo_ru_horizontal_blue_svg,
+  logo_ru_horizontal_white_png,
+  logo_ru_horizontal_white_svg,
+  logo_tr_horizontal_blue_png,
+  logo_tr_horizontal_blue_svg,
+  logo_tr_horizontal_white_png,
+  logo_tr_horizontal_white_svg,
+  logo_vi_horizontal_blue_png,
+  logo_vi_horizontal_blue_svg,
+  logo_vi_horizontal_white_png,
+  logo_vi_horizontal_white_svg,
+  logo_zh_horizontal_blue_png,
+  logo_zh_horizontal_blue_svg,
+  logo_zh_horizontal_white_png,
+  logo_zh_horizontal_white_svg,
 };
