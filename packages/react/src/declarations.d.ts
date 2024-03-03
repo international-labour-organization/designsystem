bfb69d7c9e958a3dc397c709d7d51226f69b2243
GGKapanadze <doublegkapanadze@gmail.com>
2024-03-03 16:45:43 +0400

    feat(react): added type augumentation for videojs

--- packages/react/src/declarations.d.ts
+++ packages/react/src/declarations.d.ts
@@ -1,3 +1,5 @@
+import videojs from "video.js";
+
 declare module "*.svg" {
   const content: string;
   export default content;
@@ -12,3 +14,23 @@ declare module "*.jpg" {
   const content: string;
   export default content;
 }
+
+declare module "video.js" {
+  interface VideoJsPlayerOptionsAugmentation extends videojs.PlayerOptions {
+    liveTracker: boolean;
+    textTrackSettings: boolean;
+    errorDisplay: boolean;
+    resizeManager: boolean;
+    dataSetup: {
+      techOrder: string[];
+    };
+  }
+
+  type ILOVideo = (
+    id: string | HTMLVideoElement,
+    options: VideoJsPlayerOptionsAugmentation,
+    ready?: () => void
+  ) => videojs.Player;
+}
+
+declare module "videojs-youtube" {}
