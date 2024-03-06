a76a8a7065447477a2271f2794797a47532ee3a8
justintemps <jesmith627@gmail.com>
2024-03-06 16:23:00 +0100

    fix(react): add comments to props

--- packages/react/src/components/Cards/Card.props.ts
+++ packages/react/src/components/Cards/Card.props.ts
@@ -10,81 +10,233 @@ import { LinkProps, LinkListProps } from "../LinkList/LinkList.props";
 import { ProfileProps } from "../Profile/Profile.props";
 
 export type DataCardProps = {
+  /**
+   * A line of text that appears as a small heading at the top of the card
+   */
   eyebrow?: string;
+
+  /**
+   * How big should the card be
+   */
   size?: Omit<CardSize, "standard">;
+
+  /**
+   * The image to show in the card
+   */
   image?: string;
+
+  /**
+   * Array of Content (label, copy), Files object (optional headline, array of items with label and url) and Links * * object (optional headline, array of items with label and url)
+   */
   dataset?: CardDataset;
+
+  /**
+   * Make the content appear in one or two columns. Only applies when the size is set to `wide` or `fluid`
+   */
   columns?: string;
 };
 
 export type DetailCardProps = {
+  /**
+   * A line of text that appears as a small heading above the title of the card
+   */
   eyebrow?: string;
+
+  /**
+   * The title of the card
+   */
   title: string;
+
+  /**
+   * Introductory text in the card
+   */
   intro?: string;
+
+  /**
+   * Specify the event Date, in both human and Unix format.
+   */
   date?: EventDate;
+
+  /**
+   * Information about an event
+   */
   eventdetails?: string;
+
+  /**
+   * A URL to link to
+   */
   link?: string;
+
+  /**
+   * The image to show in the card
+   */
   image?: string;
+
+  /**
+   * How big should the card be
+   */
   size?: Omit<CardSize, "standard">;
 };
 
 export type FactlistCardProps = {
+  /**
+   * The title of the card
+   */
   title: string;
+
+  /**
+   * Will render the card to appear on light or dark backgrounds
+   */
   theme?: ThemeTypes;
+
+  /**
+   * How big should the card be
+   */
   size?: Omit<CardSize, "standard">;
-  cornercut?: CardCornerType;
   list?: string[];
 };
 
 export type FeatureCardProps = {
   isvideo?: boolean;
+
+  /**
+   * A line of text that appears as a small heading above the title of the card
+   */
   eyebrow?: string;
-  title: Required<string>;
+
+  /**
+   * The title of the card
+   */
+  title: string;
+
+  /**
+   * Will render the card to appear on light or dark backgrounds
+   */
   theme?: ThemeTypes;
+
+  /**
+   * How big should the card be
+   */
   size?: Omit<CardSize, "standard">;
+
+  /**
+   * Specify the event Date, in both human and Unix format.
+   */
   date?: EventDate;
   link?: string;
   linklist?: LinkListProps;
+
+  /**
+   * The image to show in the card
+   */
   image?: string;
 };
 
 export type MultilinkCardProps = {
   isvideo?: boolean;
+
+  /**
+   * A line of text that appears as a small heading above the title of the card
+   */
   eyebrow?: string;
+
+  /**
+   * The title of the card
+   */
   title: string;
+
+  /**
+   * How big should the card be
+   */
   size?: CardSize;
   alignment?: CardAlignment;
   intro?: string;
   link?: string;
   linklist?: LinkListProps;
+
+  /**
+   * The image to show in the card
+   */
   image?: string;
 };
 
 export type PromoCardProps = {
+  /**
+   * A line of text that appears as a small heading above the title of the card
+   */
   eyebrow?: string;
-  title: Required<string>;
+
+  /**
+   * The title of the card
+   */
+  title: string;
+
+  /**
+   * Will render the card to appear on light or dark backgrounds
+   */
   theme?: ThemeTypes;
+
+  /**
+   * How big should the card be
+   */
   size?: CardSize;
+
+  /**
+   * Apply an optional corner cut to the top of the card
+   */
   cornercut?: CardCornerType;
+
+  /**
+   * Introductory text in the card
+   */
   intro?: string;
   link?: string;
   cta?: LinkProps;
 };
 
 export type StatCardProps = {
+  /**
+   * The title of the card
+   */
   title: string;
   color?: CardColor;
+
+  /**
+   * How big should the card be
+   */
   size?: Omit<CardSize, "narrow" | "wide">;
+
+  /**
+   * Introductory text in the card
+   */
   intro?: string;
   source?: LinkProps;
 };
 
 export type TextCardProps = {
+  /**
+   * A line of text that appears as a small heading above the title of the card
+   */
   eyebrow?: string;
+
+  /**
+   * The title of the card
+   */
   title: string;
+
+  /**
+   * Will render the card to appear on light or dark backgrounds
+   */
   theme?: ThemeTypes;
+
+  /**
+   * How big should the card be
+   */
   size?: Omit<CardSize, "standard">;
-  cornercut?: CardCornerType;
+
+  /**
+   * Specify the event Date, in both human and Unix format.
+   */
   date?: EventDate;
   profile: ProfileProps;
   link?: string;
@@ -99,12 +251,12 @@ export interface CardProps {
   /**
    * Eyebrow field for the card.
    */
-  eyebrow: Required<string>;
+  eyebrow: string;
 
   /**
    * Title field for the card
    */
-  title: Required<string>;
+  title: string;
 
   /**
    * Color of the stat cards, options are turquoise | green| yellow| blue. Only used for stat cards.
@@ -120,7 +272,7 @@ export interface CardProps {
    * Type - Sets the type of card to render.
      Option keys: feature, text, detail, promo, multilink, data, stat, factlist
    */
-  type: Required<CardTypes>;
+  type: CardTypes;
 
   /**
    * Size of the cards (usually reduces padding). `Wide` on Multilink and Feature cause a two column structure above a desktop breakpoint. Possible options: Wide | Standard | Narrow.
@@ -128,7 +280,7 @@ export interface CardProps {
   size?: CardSize;
 
   /**
-   * Specify whether the card has a cut corner. The only cards that use this setting are `Promo` and `Factlist`. Possible options: cornercut | corner.
+   * Specify whether the card has a cut corner. The only card that uses this setting are `Promo`.
    */
   cornercut?: CardCornerType;
 
