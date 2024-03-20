a7684eaa7fc1ae5c8fcd2b55e13ae65bf153d0ed
Shashika6 <Shashikaboteju3@gmail.com>
2024-03-20 15:19:10 +0530

    fix(react) - refactor prop descriptions

--- packages/react/src/components/Cards/CardGroup/CardGroup.props.ts
+++ packages/react/src/components/Cards/CardGroup/CardGroup.props.ts
@@ -22,97 +22,48 @@ export interface CommonCardGroupProps {
 
 export interface StatCardGroup extends CommonCardGroupProps {
   /**
-   * Type of card group, always "stat" for stat card groups.
+   * Represents different types of card groups:
    */
   type: "stat";
 
   /**
-   * An array of statcard objects.
+   * An array of card objects corresponding to the respective card type:
    */
   cards: StatCardProps[];
 }
 
 export interface MultilinkCardGroup extends CommonCardGroupProps {
-  /**
-   * Type of card group, always "multilink" for multi link card groups.
-   */
   type: "multilink";
-
-  /**
-   * An array of multilinkcard objects
-   */
   cards: MultilinkCardProps[];
 }
 
 export interface TextCardGroup extends CommonCardGroupProps {
-  /**
-   * Type of card group, always "text" for text card groups.
-   */
   type: "text";
-
-  /**
-   * An array of textcard objects.
-   */
   cards: TextCardProps[];
 }
 
 export interface PromoCardGroup extends CommonCardGroupProps {
-  /**
-   * Type of card group, always "promo" for promo card groups.
-   */
   type: "promo";
-
-  /**
-   * An array of promocard objects.
-   */
   cards: PromoCardProps[];
 }
 
 export interface FeatureCardGroup extends CommonCardGroupProps {
-  /**
-   * Type of card group, always "feature" for feature card groups.
-   */
   type: "feature";
-
-  /**
-   * An array of featurecard objects.
-   */
   cards: FeatureCardProps[];
 }
 
 export interface DetailCardGroup extends CommonCardGroupProps {
-  /**
-   * Type of card group, always "detail" for detail card groups.
-   */
   type: "detail";
-
-  /**
-   * An array of detailcard objects.
-   */
   cards: DetailCardProps[];
 }
 
 export interface FactlistCardGroup extends CommonCardGroupProps {
-  /**
-   * Type of card group, always "factlist" for factlist card groups.
-   */
   type: "factlist";
-
-  /**
-   * An array of factlistcard objects.
-   */
   cards: FactlistCardProps[];
 }
 
 export interface DataCardGroup extends CommonCardGroupProps {
-  /**
-   * Type of card group, always "data" for data card groups.
-   */
   type: "data";
-
-  /**
-   * An array of datacard objects.
-   */
   cards: DataCardProps[];
 }
 
