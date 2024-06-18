412dff2da2c9ad6e7f17062c7079afae728eec89
Shashika6 <Shashikaboteju3@gmail.com>
2024-06-19 02:09:43 +0530

    fix(test) - refactor tooltip e2e class

--- packages/twig/cypress/e2e/feedback/tooltip.cy.js
+++ packages/twig/cypress/e2e/feedback/tooltip.cy.js
@@ -7,7 +7,7 @@ describe("tooltip", () => {
   it("renders the tooltip correctly and checks if popover works on hover", () => {
     cy.get("@tooltipSection").within(() => {
       cy.get(".ilo--tooltip").should("not.be.visible");
-      cy.get(".ilo--tooltip--wrapper__icon")
+      cy.get(".ilo--tooltip--wrapper")
         .should("exist")
         .invoke("show")
         .trigger("mouseover", { force: true });
