7810b4ada9400c4bfa55bdef3ad3abee94434fc8
GGKapanadze <doublegkapanadze@gmail.com>
2024-07-01 16:56:15 +0400

    feat(maestro): initialised maestro package

---
+++ packages/maestro/vite.config.ts
@@ -0,0 +1,21 @@
+import { builtinModules } from "node:module";
+
+import dts from "vite-plugin-dts";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [dts()],
+  build: {
+    lib: {
+      entry: ["src/main.ts", "src/vite.ts", "src/sb.ts"],
+      formats: ["es"],
+    },
+    target: "esnext",
+    outDir: "lib",
+    rollupOptions: {
+      external: builtinModules
+        .map((item) => `node:${item}`)
+        .concat(builtinModules),
+    },
+  },
+});
