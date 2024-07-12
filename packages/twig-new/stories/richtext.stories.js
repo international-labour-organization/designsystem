8d1f05201bd6d0199961b1658f9c8c18fe4c0649
justintemps <jesmith627@gmail.com>
2024-07-12 11:39:29 +0200

    feat(migrate-twig): readmore & richtext

---
+++ packages/twig-new/stories/richtext.stories.js
@@ -0,0 +1,16 @@
+import Richtext from "../src/components/richtext/richtext.twig";
+import RichtextPatterns from "../src/components/richtext/richtext.pattern.yml";
+import { Maestro } from "@ilo-org/maestro";
+
+const story = Maestro.create(Richtext, RichtextPatterns);
+
+const Meta = {
+  title: "Components/Content/Rich Text",
+  tags: ["autodocs"],
+  ...story.meta,
+};
+
+const [Default] = story.stories;
+
+export default Meta;
+export { Default };
