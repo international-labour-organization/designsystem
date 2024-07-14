8d47e2c1ca5485ab99ba5c76487a972d3cf28e94
GGKapanadze <doublegkapanadze@gmail.com>
2024-07-15 00:00:46 +0400

    refactor(twig): ported text card

---
+++ packages/twig-new/stories/card_text.stories.js
@@ -0,0 +1,16 @@
+import CardText from "../src/components/card_text/card_text.twig";
+import CardTextPatterns from "../src/components/card_text/card_text.pattern.yml";
+import { Maestro } from "@ilo-org/maestro";
+
+const story = Maestro.create(CardText, CardTextPatterns);
+
+const Meta = {
+  title: "Components/Cards/Text Card",
+  tags: ["autodocs"],
+  ...story.meta,
+};
+
+const [Default] = story.stories;
+
+export default Meta;
+export { Default };
