aa9980d42cfc1d69406ae6bdaf311a50ac0b52b6
GGKapanadze <doublegkapanadze@gmail.com>
2024-07-18 15:41:36 +0400

    refactor(twig): ported modal component

---
+++ packages/twig-new/stories/modal.stories.js
@@ -0,0 +1,16 @@
+import Modal from "../src/components/modal/modal.twig";
+import ModalPatterns from "../src/components/modal/modal.pattern.yml";
+import { Maestro } from "@ilo-org/maestro";
+
+const story = Maestro.create(Modal, ModalPatterns);
+
+const Meta = {
+  title: "Components/Feedback/Modal",
+  tags: ["autodocs"],
+  ...story.meta,
+};
+
+const [Default] = story.stories;
+
+export default Meta;
+export { Default };
