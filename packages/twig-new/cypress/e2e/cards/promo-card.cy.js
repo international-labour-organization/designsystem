4dab229333b51cb151fe9d4e0b49d99d64ae7bc2
Shashika6 <Shashikaboteju3@gmail.com>
2024-07-23 13:06:58 +0530

    chore(twig) - setup cypress tests locally

---
+++ packages/twig-new/cypress/e2e/cards/promo-card.cy.js
@@ -0,0 +1,11 @@
+describe("Promo card", () => {
+  it("renders the promo card correctly", () => {
+    cy.visit("patterns/promocard");
+    cy.getPreview("promocard").within(() => {
+      cy.contains("Podcast");
+      cy.contains("Can digital technology be an equality machine?");
+      cy.contains("A toxic combination");
+      cy.contains("Read the press release");
+    });
+  });
+});
