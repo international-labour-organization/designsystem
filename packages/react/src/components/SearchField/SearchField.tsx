33e6e5661b52cbc201c1de1ae961587f02ba515f
Shashika6 <Shashikaboteju3@gmail.com>
2024-08-11 19:01:16 +0530

    feat(react-twig): focus search input field on button click

--- packages/react/src/components/SearchField/SearchField.tsx
+++ packages/react/src/components/SearchField/SearchField.tsx
@@ -1,83 +1,82 @@
-import { FC, useState } from "react";
+import { FC, useState, forwardRef } from "react";
 import classNames from "classnames";
 import useGlobalSettings from "../../hooks/useGlobalSettings";
 import { SearchFieldProps } from "./SearchField.props";
 import { Input } from "../Input";
 import { Icon } from "../Icon";
 
-const SearchField: FC<SearchFieldProps> = ({
-  action,
-  callback,
-  className,
-  input,
-}) => {
-  const [searchValue, setSearchValue] = useState("");
+const SearchField: FC<
+  SearchFieldProps & React.RefAttributes<HTMLInputElement>
+> = forwardRef<HTMLInputElement, SearchFieldProps>(
+  ({ action, callback, className, input }, ref) => {
+    const [searchValue, setSearchValue] = useState("");
+    const { prefix } = useGlobalSettings();
+    const baseClass = `${prefix}--searchfield`;
+    const buttonClass = `${baseClass}--button`;
+    const clearButtonClass = `${baseClass}--clear-button ${
+      searchValue.trim() !== "" && "show"
+    }`;
+    const fieldSetClass = `${prefix}--fieldset`;
 
-  const { prefix } = useGlobalSettings();
-  const baseClass = `${prefix}--searchfield`;
-  const buttonClass = `${baseClass}--button`;
-  const clearButtonClass = `${baseClass}--clear-button ${
-    searchValue.trim() !== "" && "show"
-  }`;
-  const fieldSetClass = `${prefix}--fieldset`;
+    const SearchFieldClasses = classNames(className, {
+      [baseClass]: true,
+      [`haslabel`]: input?.label,
+    });
 
-  const SearchFieldClasses = classNames(className, {
-    [baseClass]: true,
-    [`haslabel`]: input?.label,
-  });
+    const handleClick: React.MouseEventHandler<HTMLInputElement> = (e) => {
+      if (callback) {
+        callback(e);
+      }
+    };
 
-  const handleClick: React.MouseEventHandler<HTMLInputElement> = (e) => {
-    if (callback) {
-      callback(e);
+    // handle click for clear button in search
+    const handleClearButtonClick: React.MouseEventHandler<
+      HTMLSpanElement
+    > = () => {
+      setSearchValue("");
+    };
+
+    // Update search value on input
+    const onKeyPress: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+      setSearchValue(e.target.value);
+    };
+
+    const inputHasType = !!input?.type;
+
+    if (!inputHasType) {
+      throw new Error("SearchField: Input must have type prop");
     }
-  };
 
-  // handle click for clear button in search
-  const handleClearButtonClick: React.MouseEventHandler<
-    HTMLInputElement
-  > = () => {
-    setSearchValue("");
-  };
-
-  // Update search value on input
-  const onKeyPress: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    setSearchValue(e.target.value);
-  };
-
-  const inputHasType = !!input?.type;
-
-  if (!inputHasType) {
-    throw new Error("SearchField: Input must have type prop");
-  }
-
-  return inputHasType ? (
-    <form className={SearchFieldClasses} action={action}>
-      <div className={fieldSetClass}>
-        <Input
-          id={input?.id}
-          name={input?.name}
+    return inputHasType ? (
+      <form className={SearchFieldClasses} action={action}>
+        <div className={fieldSetClass}>
+          <Input
+            id={input?.id}
+            name={input?.name}
+            disabled={input?.disabled}
+            callback={onKeyPress}
+            error={input?.error}
+            helper={input?.helper}
+            label={input?.label}
+            placeholder={input?.placeholder}
+            type={input?.type}
+            value={searchValue}
+            className={`${prefix}--input`}
+            ref={ref}
+          />
+          <span onClick={handleClearButtonClick} className={clearButtonClass}>
+            <Icon name="close" hidden={true} />
+          </span>
+        </div>
+        <input
+          className={buttonClass}
           disabled={input?.disabled}
-          callback={onKeyPress}
-          error={input?.error}
-          helper={input?.helper}
-          label={input?.label}
-          placeholder={input?.placeholder}
-          type={input?.type}
-          value={searchValue}
-          className={`${prefix}--input`}
+          type="submit"
+          onClick={handleClick}
         />
-        <span onClick={handleClearButtonClick} className={clearButtonClass}>
-          <Icon name="close" hidden={true} />
-        </span>
-      </div>
-      <input
-        className={buttonClass}
-        disabled={input?.disabled}
-        type="submit"
-        onClick={handleClick}
-      />
-    </form>
-  ) : null;
-};
+      </form>
+    ) : null;
+  }
+);
 
 export default SearchField;
