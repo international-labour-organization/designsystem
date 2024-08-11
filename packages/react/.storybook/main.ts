e01ae3776001143cd6e425616149f7900972b7f0
GGKapanadze <doublegkapanadze@gmail.com>
2024-08-11 15:41:01 +0400

    refactor(react): ported storybook configs to object export

--- packages/react/.storybook/main.ts
+++ packages/react/.storybook/main.ts
@@ -1,41 +1,41 @@
-export const core = {
-  disableTelemetry: true,
-  builder: "@storybook/builder-vite",
-};
+import { dirname, join } from "path";
+import type { StorybookConfig } from "@storybook/react-vite";
 
-export const framework = {
-  name: "@storybook/react-vite",
-};
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
 
-export const stories = [
-  "../src/stories/**/*.mdx",
-  "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
-];
-
-export const docs = {
-  defaultName: "Overview",
-  autodocs: "tag",
-};
-
-export const staticDirs = [
-  "../public",
-  { from: "../node_modules/@ilo-org/fonts/font-css", to: "/fonts" },
-];
-
-export const addons = [
-  "@storybook/addon-links",
-  "@storybook/addon-essentials",
-  "@storybook/addon-a11y",
-  "@storybook/addon-styling",
-  "storybook-addon-rtl-direction",
-];
-
-export const typescript = {
-  reactDocgen: "react-docgen-typescript",
-  reactDocgenTypescriptOptions: {
-    compilerOptions: {
-      allowSyntheticDefaultImports: true,
-      esModuleInterop: true,
+const config: StorybookConfig = {
+  stories: [
+    "../src/stories/**/*.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  staticDirs: [
+    "../public",
+    { from: "../node_modules/@ilo-org/fonts/font-css", to: "/fonts" },
+  ],
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  addons: [
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("storybook-addon-rtl"),
+  ],
+  framework: {
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {},
+  },
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: true,
+        esModuleInterop: true,
+      },
     },
   },
 };
+
+export default config;
--- packages/react/.storybook/preview.tsx
+++ packages/react/.storybook/preview.tsx
@@ -1,77 +1,9 @@
 import { GlobalProvider } from "../src/components/GlobalProvider";
 import { themeprefix } from "@ilo-org/themes/tokens/theme/base.json";
-import { Decorator } from "@storybook/react";
-import { theme } from "./theme";
+import { Preview } from "@storybook/react";
+import theme from "./theme";
 import "./styles.scss";
 import { brand } from "@ilo-org/themes/tokens/brand/base.json";
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    expanded: true,
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-  options: {
-    storySort: {
-      method: "alphabetical",
-      order: ["Welcome", "GettingStarted"],
-      locales: "en-US",
-    },
-  },
-  backgrounds: {
-    values: [
-      { name: "light", value: "white" },
-      { name: "dark", value: brand["ilo-dark-blue"].value },
-    ],
-  },
-  previewTabs: {
-    "storybook/docs/panel": { index: -1 },
-    canvas: { title: "Code", hidden: false },
-  },
-  viewMode: "docs",
-  layout: "padded",
-  docs: {
-    theme: theme,
-    source: {
-      excludeDecorators: true,
-    },
-  },
-};
-
-export const decorators: Decorator[] = [
-  // Providces the GlobalProvider context to all stories
-  (Story) => (
-    <GlobalProvider prefix={themeprefix.value}>
-      <Story />
-    </GlobalProvider>
-  ),
-  // Wraps components in story mode
-  (Story, ctx) => {
-    // Should this story be rendered in full width?
-    const fullWidthStories = ["footer", "localnav", "navigation", "hero"];
-    const isFullWidth = fullWidthStories.some((story) =>
-      ctx.title.toLocaleLowerCase().includes(story)
-    );
-
-    if (ctx.viewMode === "story") {
-      if (isFullWidth) {
-        return <Story />;
-      }
-
-      return (
-        <div className="story-decorator-wrapper ">
-          <div className="story-decorator">
-            <Story />
-          </div>
-        </div>
-      );
-    }
-    return <Story />;
-  },
-];
 
 export const globalTypes = {
   rtlDirection: {
@@ -79,3 +11,73 @@ export const globalTypes = {
     defaultValue: false,
   },
 };
+
+const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <GlobalProvider prefix={themeprefix.value}>
+        <Story />
+      </GlobalProvider>
+    ),
+    // Wraps components in story mode
+    (Story, ctx) => {
+      // Should this story be rendered in full width?
+      const fullWidthStories = ["footer", "localnav", "navigation", "hero"];
+      const isFullWidth = fullWidthStories.some((story) =>
+        ctx.title.toLocaleLowerCase().includes(story)
+      );
+
+      if (ctx.viewMode === "story") {
+        if (isFullWidth) {
+          return <Story />;
+        }
+
+        return (
+          <div className="story-decorator-wrapper ">
+            <div className="story-decorator">
+              <Story />
+            </div>
+          </div>
+        );
+      }
+      return <Story />;
+    },
+  ],
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      expanded: true,
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    options: {
+      storySort: {
+        method: "alphabetical",
+        order: ["ILO Design System for React", "Getting Started"],
+        locales: "en-US",
+      },
+    },
+    backgrounds: {
+      values: [
+        { name: "light", value: "white" },
+        { name: "dark", value: brand["ilo-dark-blue"].value },
+      ],
+    },
+    previewTabs: {
+      "storybook/docs/panel": { index: -1 },
+      canvas: { title: "Code", hidden: false },
+    },
+    viewMode: "docs",
+    layout: "padded",
+    docs: {
+      theme: theme,
+      source: {
+        excludeDecorators: true,
+      },
+    },
+  },
+};
+
+export default preview;
