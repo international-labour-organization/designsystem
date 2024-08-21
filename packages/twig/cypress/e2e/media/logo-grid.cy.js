f675edcae17d85d9b2dabd1bd81b3a8bd6832c89
justintemps <jesmith627@gmail.com>
2024-08-21 17:30:06 +0200

    tests(twig): logo grid

--- packages/twig/cypress/e2e/media/logo-grid.cy.js
+++ packages/twig/cypress/e2e/media/logo-grid.cy.js
@@ -1,17 +1,19 @@
+const { isExportDeclaration } = require("typescript");
+
 describe("logo grid", () => {
   beforeEach(() => {
     cy.visit("/patterns/logogrid");
     cy.getPreview("logogrid").first().as("logoGridSection");
   });
 
-  it("renders the logo grid correctly", () => {
+  it("renders all of the logo items", () => {
     cy.get("@logoGridSection").within(() => {
-      cy.get(".ilo--logo-grid--logos").find("a").should("have.length", 6);
+      cy.get(".ilo--logo-grid--logos").find("a").should("have.length", 11);
       cy.get(".ilo--logo-grid--item").should("exist");
     });
   });
 
-  it("checks if logo grid functions as expected", () => {
+  it("has all of the expected attributes", () => {
     cy.get(".ilo--logo-grid--item")
       .should("have.attr", "href")
       .and("not.be.empty");
@@ -21,5 +23,28 @@ describe("logo grid", () => {
       .within(() => {
         cy.get("img").should("have.attr", "src").and("not.be.empty");
       });
+  });
+
+  it("at mobile breakpoint, shows items taking up full container width", () => {
+    cy.viewport("iphone-6");
+    cy.get(".ilo--logo-grid--logos").then(($container) => {
+      const containerWidth = $container.outerWidth();
+      cy.get(".ilo--logo-grid--item").each(($item) => {
+        const itemWidth = $item.outerWidth();
+        expect(itemWidth).to.equal(containerWidth);
+      });
+    });
+  });
+
+  it("at small breakpoint, shows items taking up half container width", () => {
+    cy.viewport("iphone-6+");
+    cy.get(".ilo--logo-grid--logos").then(($container) => {
+      const containerWidth = $container.outerWidth();
+      cy.get(".ilo--logo-grid--item").each(($item) => {
+        const itemWidth = $item.outerWidth();
+        // Item takes up half the width minus the grid gap
+        expect(itemWidth).to.equal(containerWidth / 2 - 16);
+      });
+    });
   });
 });
