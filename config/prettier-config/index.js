f405ebc2a6f5289e90c87e2702ac6878224af931
GGKapanadze <doublegkapanadze@gmail.com>
2024-09-10 14:59:14 +0400

    build(prettier-config): added esmodule export

--- config/prettier-config/index.js
+++ config/prettier-config/index.js
@@ -1,9 +1,12 @@
-"use strict";
-
-module.exports = {
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
   endOfLine: "lf",
   singleQuote: false,
   tabWidth: 2,
   trailingComma: "es5",
   printWidth: 80,
 };
+
+export default config;
