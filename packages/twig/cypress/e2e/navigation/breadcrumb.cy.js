3bda8c3d97c769c67320b918dc8b0a6a97c5ec07
justintemps <jesmith627@gmail.com>
2024-09-16 12:54:23 +0200

    fix(twig): breadcrumb showing the last element twice

--- packages/twig/cypress/e2e/navigation/breadcrumb.cy.js
+++ packages/twig/cypress/e2e/navigation/breadcrumb.cy.js
@@ -4,17 +4,34 @@ describe("breadcrumb", () => {
     cy.getPreview("breadcrumb").first().as("breadcrumbSection");
   });
 
-  it("renders the breadcrumb correctly", () => {
-    cy.get("@breadcrumbSection").within(() => {
-      cy.get(".ilo--breadcrumb--items")
-        .should("not.contain", "Link One")
-        .find("ol")
-        .should("exist");
+  it("should render five links in desktop view", () => {
+    cy.viewport(1280, 720);
+    cy.get(".ilo--breadcrumb--link").should("have.length", 5);
+  });
 
-      cy.contains("Link Two");
-      cy.contains("Link Three");
-      cy.contains("Link Four");
-      cy.contains("Link Five");
-    });
+  it("should render the first link with an :after pseudo-element that has a background image", () => {
+    cy.get(".ilo--breadcrumb--item__first .ilo--breadcrumb--link").then(
+      ($el) => {
+        const afterContent = window.getComputedStyle($el[0], "::after");
+        expect(afterContent.getPropertyValue("background-image")).to.not.equal(
+          "none"
+        );
+      }
+    );
+  });
+
+  it("should not render context menu links in mobile view initially", () => {
+    cy.viewport(375, 667);
+    cy.get(".ilo--breadcrumb--context--menu").should("be.hidden");
+  });
+
+  it("should have the correct number of links in the context menu", () => {
+    cy.get(".ilo--context-menu--item").should("have.length", 3);
+  });
+
+  it("should render context menu links when the context button is clicked in mobile view", () => {
+    cy.viewport(375, 1200);
+    cy.get(".ilo--breadcrumb--context--button").click();
+    cy.get(".ilo--breadcrumb--context--menu").should("not.be.hidden");
   });
 });
