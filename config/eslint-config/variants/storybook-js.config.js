b8bebab4d2d73cffa4d74b4b5a62bc37fbe2f9b5
GGKapanadze <doublegkapanadze@gmail.com>
2024-09-17 16:02:44 +0400

    feat(eslint-config): added containre for future storybook config

---
+++ config/eslint-config/variants/storybook-js.config.js
@@ -0,0 +1,4 @@
+// TODO: wait for the eslint-plugin-storybook v9 support https://github.com/storybookjs/eslint-plugin-storybook/issues/157
+import baseJsConfig from "./base-js.config.js";
+
+export default [...baseJsConfig];
