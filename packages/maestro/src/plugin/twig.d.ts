d30bf2c5c78192597a2180bc4e2d4e12e7cd1604
GGKapanadze <doublegkapanadze@gmail.com>
2024-09-18 16:28:50 +0400

    chore(maestro): adjusted codebase to a new eslint config

--- packages/maestro/src/plugin/twig.d.ts
+++ packages/maestro/src/plugin/twig.d.ts
@@ -1,9 +1,9 @@
-type PluginParams = {
+interface PluginParams {
   namespaces: Record<string, string>;
   functions?: Record<string, unknown>;
   globalContext?: Record<string, unknown>;
   dynamics?: string[];
-};
+}
 function twig(args: PluginParams): PluginOption;
 
 export default twig;
--- packages/maestro/src/vite.ts
+++ packages/maestro/src/vite.ts
@@ -1,16 +1,18 @@
 import Twig from "twig";
 import yaml from "@modyfi/vite-plugin-yaml";
+import type { Plugin } from "vite";
 
 import twig from "./plugin/twig";
 
-type Options = {
+interface Options {
   namespaces: Record<string, string>;
   globals?: Record<string, unknown>;
   dynamics?: string[];
-};
+}
 
-function UIPatterns(options: Options) {
-  return [
+function UIPatterns(options: Options): Plugin[] {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const plugins: Plugin[] = [
     yaml(),
     twig({
       namespaces: options.namespaces,
@@ -18,13 +20,15 @@ function UIPatterns(options: Options) {
       dynamics: options.dynamics,
       functions: {
         boolval: (instance: typeof Twig) =>
-          //@ts-expect-error typedefintions are wrong
+          // @ts-expect-error - twig preprocessor wants it to be a function
           instance.extendFilter("boolval", (value: string) =>
             value === "false" ? false : !!value
           ),
       },
     }),
   ];
+
+  return plugins;
 }
 
 export { UIPatterns };
