11459adc9c28a84753a34ca4daa000023f3c51c8
GGKapanadze <doublegkapanadze@gmail.com>
2024-09-20 16:39:33 +0400

    buil(eslint-config): removed storybook config

--- config/eslint-config/index.js
+++ config/eslint-config/index.js
@@ -1,13 +1,11 @@
 import js from "./variants/base-js.config.js";
 import ts from "./variants/base-ts.config.js";
 import react from "./variants/react.config.js";
-import storybook from "./variants/storybook-js.config.js";
 
 const configs = {
   js,
   recommended: ts,
   react,
-  storybook,
 };
 
 export default configs;
--- config/eslint-config/variants/storybook-js.config.js
+++
@@ -1,4 +0,0 @@
-// TODO: wait for the eslint-plugin-storybook v9 support https://github.com/storybookjs/eslint-plugin-storybook/issues/157
-import baseJsConfig from "./base-js.config.js";
-
-export default [...baseJsConfig];
