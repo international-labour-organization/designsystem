df2cd4ddd853fcaa987a6de83143649dce150641
justintemps <jesmith627@gmail.com>
2024-10-01 23:07:56 +0200

    docs(twig): richtext adjust

--- packages/twig/stories/richtext.stories.js
+++ packages/twig/stories/richtext.stories.js
@@ -9,9 +9,15 @@ const component = `
 <h2>Usage</h2>
 <p>The ILO Design System doesn't include font styles and spacing in its base stylesheet. That's to make the Design System easier to integrate incrementally, so you can include our CSS without having to worry about it conflicting with your own font styles.</p>
 <p>To use this component, simply pass a string of (valid) HTML to its \`content\` property. Default styles and spacing rules will be applied to all text and container elements.</p>
+<h2>Blockquotes</h2>
+<p>Blockquotes apply some of their own styling to children.</p>
+<ul>
+<li>The first child should be in \`<p>\` and includes the content of the quote.</li>
+<li>The first child should be a \`<cite>\` and should attribute the quote to a source.</li>
+</ul>
 <h2>Embedded content</h2>
-<p>The Richtext does not apply spacing directly to iframes. Instead, wrap iframes in an \`<article>\`</p> tag to ensure they are spaced correctly.</p>
-<p>If the embed is a video or some other piece of content with a 16:9 aspect ratio that you want to behave responsively, use the following utility class in the iframe wrapper: \`<article class="responsive-video-embed">\`</p>
+<p>The Richtext does not apply spacing directly to iframes. Instead, wrap iframes in an \`<article>\` tag to ensure they are spaced correctly.</p>
+<p>If the embed is a video or some other piece of content with a 16:9 aspect ratio that you want to behave responsively, apply the \`.responsive-video-embed\` class to the iframe wrapper.</p>
 <h2>Example</h2>
 `;
 
