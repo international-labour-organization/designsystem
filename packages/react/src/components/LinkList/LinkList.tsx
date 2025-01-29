cd2f79c48a40af57e000727e00de024ca16a2df6
GGKapanadze <doublegkapanadze@gmail.com>
2025-01-29 18:28:04 +0400

    refactor(react): reworked link list component

--- packages/react/src/components/LinkList/LinkList.tsx
+++ packages/react/src/components/LinkList/LinkList.tsx
@@ -1,60 +1,106 @@
-import { FC } from "react";
+import { forwardRef } from "react";
 import classNames from "classnames";
 import useGlobalSettings from "../../hooks/useGlobalSettings";
-import { LinkListProps } from "./LinkList.props";
+import { LinkListThemes } from "../../types";
 
-const LinkList: FC<LinkListProps> = ({
-  className,
-  headline,
-  linkgroup,
-  theme,
-}) => {
-  const { prefix } = useGlobalSettings();
-  const baseClass = `${prefix}--link-list`;
-  const linkListClasses = classNames(className, {
-    [baseClass]: true,
-    [`${baseClass + "--" + (theme && theme === "dark" ? theme : "light")}`]:
-      true,
-  });
+export type LinkProps = {
+  /**
+   * Specify if this link should indent and have a triangle icon
+   */
+  indented?: boolean;
 
-  return (
-    <div className={linkListClasses}>
-      {headline && <h3 className={`${baseClass}--headline`}>{headline}</h3>}
-      <ul className={`${baseClass}--linkgroups`}>
-        {linkgroup &&
-          linkgroup.map((linkset, i) => (
-            <li
-              className={`${baseClass}--linkgroups-item`}
-              key={`${baseClass}--linkgroups-item-${i}`}
-            >
-              {linkset.headline && (
+  /**
+   * Specify the label for this link
+   */
+  label: string;
+
+  /**
+   * Specify the url for this link
+   */
+  url: string;
+};
+
+export type LinkGroupProps = {
+  /**
+   * Specify an optional headline for this link group
+   */
+  headline?: string;
+
+  /**
+   * Specify the links to be displayed in this link group
+   */
+  links: Array<LinkProps>;
+};
+
+export type LinkListProps = {
+  /**
+   * Specify an optional className to be added to your Link List component.
+   */
+  className?: string;
+
+  /**
+   * Specify a headline for the the Link List
+   */
+  headline?: string;
+
+  /**
+   * Specify the links to be displayed in the Link List
+   */
+  linkgroup: Array<LinkGroupProps>;
+
+  /**
+   * Specify the theme for the Link List
+   */
+  theme?: LinkListThemes;
+};
+
+const LinkList = forwardRef<HTMLDivElement, LinkListProps>(
+  ({ className, headline, linkgroup, theme = "light" }, ref) => {
+    const { prefix } = useGlobalSettings();
+
+    const baseClass = `${prefix}--link-list`;
+
+    return (
+      <div
+        ref={ref}
+        className={classNames(baseClass, className, {
+          [`${baseClass}--theme--${theme}`]: theme,
+        })}
+        data-loadcomponent="LinkList"
+        data-prefix={prefix}
+      >
+        {headline && <h3 className={`${baseClass}--headline`}>{headline}</h3>}
+        <ul className={`${baseClass}--linkgroups`}>
+          {linkgroup.map((group, index) => (
+            <li key={index} className={`${baseClass}--linkgroups-item`}>
+              {group.headline && (
                 <h4 className={`${baseClass}--links--headline`}>
-                  {linkset.headline}
+                  {group.headline}
                 </h4>
               )}
               <ul className={`${baseClass}--links`}>
-                {linkset.links &&
-                  linkset.links.map((link, i) => (
-                    <li
-                      className={`${baseClass}--links--item${
-                        link.indented ? " indented" : ""
-                      }`}
-                      key={`${baseClass}--links--item-${i}`}
-                    >
-                      <a href={link.url} className={`${baseClass}--link`}>
-                        <span className={`${baseClass}--label`}>
-                          {link.label}
-                        </span>
-                        <span className={`${baseClass}--icon`} />
-                      </a>
-                    </li>
-                  ))}
+                {group.links.map((link, linkIndex) => (
+                  <li
+                    key={linkIndex}
+                    className={classNames(`${baseClass}--links--item`, {
+                      indented: link.indented,
+                    })}
+                  >
+                    <a href={link.url} className={`${baseClass}--link`}>
+                      <span className={`${baseClass}--label`}>
+                        {link.label}
+                      </span>
+                      <span className={`${baseClass}--icon`}></span>
+                    </a>
+                  </li>
+                ))}
               </ul>
             </li>
           ))}
-      </ul>
-    </div>
-  );
-};
+        </ul>
+      </div>
+    );
+  }
+);
 
-export default LinkList;
+export { LinkList };
--- packages/react/src/components/LinkList/index.ts
+++ packages/react/src/components/LinkList/index.ts
@@ -1,1 +1,1 @@
-export { default as LinkList } from "./LinkList";
+export * from "./LinkList";
