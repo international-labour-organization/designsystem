8e29833a05d21efc7497925995ac2a71a0d8f343
GGKapanadze <doublegkapanadze@gmail.com>
2025-02-02 22:08:23 +0400

    refactor(react): reworked the button component

--- packages/react/src/components/Button/Button.tsx
+++ packages/react/src/components/Button/Button.tsx
@@ -1,78 +1,120 @@
-import { FC } from "react";
+import { ButtonHTMLAttributes, forwardRef, ReactNode } from "react";
 import classNames from "classnames";
+
 import useGlobalSettings from "../../hooks/useGlobalSettings";
-import { ButtonProps } from "./Button.props";
 import { Link } from "../Link";
 import { Icon } from "../Icon";
+import { ButtonTypes, PositionTypes, SizeTypes } from "../../types";
 
-const Button: FC<ButtonProps> = ({
-  callback,
-  className,
-  disabled = false,
-  icon,
-  icononly,
-  iconPosition,
-  kind = "button",
-  label,
-  opensmodal,
-  size = "large",
-  target = "",
-  type = "primary",
-  url,
-  style,
-  name,
-}) => {
-  const { prefix } = useGlobalSettings();
-  const baseClass = `${prefix}--button`;
-  const hasURL = !!url;
-  const icoPos = iconPosition || "left";
-
-  const ButtonClasses = classNames(className, {
-    [baseClass]: true,
-    [`${baseClass}__${size}`]: size,
-    [`${baseClass}__${type}`]: type,
-    [`${baseClass}--icon ${baseClass}--icon--position__${icoPos}`]:
-      icon && !icononly,
-    [`${baseClass}--icon ${baseClass}--icon--only`]: icon && icononly,
-  });
+export type ButtonProps = Omit<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  "type"
+> & {
+  /**
+   * Specify the content of your Button.
+   */
+  children?: ReactNode;
 
   /**
-   * On click, if there is a callback, call it
+   * Specify an optional className to be added to your Button.
    */
-  const handleClick = (e: React.MouseEvent<Element, MouseEvent>) => {
-    if (callback) {
-      callback(e);
-    }
-  };
+  className?: string;
 
-  return (
-    <>
-      {hasURL ? (
-        <Link
-          className={ButtonClasses}
-          target={target}
-          url={url}
-          label={label}
-          style={style}
-        >
-          {icon && <Icon name={icon} hidden={true} />}
-        </Link>
-      ) : (
-        <button
-          className={ButtonClasses}
-          onClick={(e) => handleClick(e)}
-          disabled={disabled}
-          type={kind}
-          aria-haspopup={opensmodal ? `dialog` : false}
-          style={style}
-          name={name}
-        >
-          {label && <span className="button__label">{label}</span>}
-          {icon && <Icon name={icon} hidden={true} />}
-        </button>
-      )}
-    </>
-  );
+  /**
+   * The size of the button: `small`, `medium`, `large`
+   */
+  size?: SizeTypes;
+
+  /**
+   * Types of the button: `primary`, `secondary`, `tertiary`, `ghost`
+   */
+  type?: ButtonTypes;
+
+  /**
+   * Specify if the button should be rendered as link
+   */
+  link?: {
+    /**
+     * Specify the url for the Button's href
+     */
+    url: string;
+
+    /**
+     * Specify the label for the Button
+     */
+    label: string;
+
+    /**
+     * Specify the target for when Button is really a link
+     */
+    target?: string;
+  };
+  icon?: {
+    name: string;
+    position?: PositionTypes;
+    only?: boolean;
+  };
 };
 
-export default Button;
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      children,
+      className,
+      disabled = false,
+      icon,
+      size = "large",
+      type = "primary",
+      style,
+      name,
+      link,
+      ...props
+    },
+    ref
+  ) => {
+    const { prefix } = useGlobalSettings();
+
+    const baseClass = `${prefix}--button`;
+
+    const rootClasses = classNames(className, {
+      [baseClass]: true,
+      [`${baseClass}__${size}`]: size,
+      [`${baseClass}__${type}`]: type,
+      [`${baseClass}--icon ${baseClass}--icon--position__${icon?.position || "left"}`]:
+        icon && !icon.only,
+      [`${baseClass}--icon ${baseClass}--icon--only`]: icon && icon.only,
+    });
+
+    if (link) {
+      return (
+        <Link
+          className={rootClasses}
+          target={link.target}
+          url={link.url}
+          label={link.label}
+          style={style}
+          // Link has no disabled state
+        >
+          {icon && <Icon name={icon.name} hidden={true} />}
+          {children}
+        </Link>
+      );
+    }
+
+    return (
+      <button
+        ref={ref}
+        className={rootClasses}
+        disabled={disabled}
+        style={style}
+        name={name}
+        {...props}
+      >
+        <span className="button__label">{children}</span>
+        {icon && <Icon name={icon.name} hidden={true} />}
+      </button>
+    );
+  }
+);
+
+export { Button };
