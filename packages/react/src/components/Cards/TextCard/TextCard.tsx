b4acc22c85bed7e1c5953d725514182203b5bd0f
GGKapanadze <doublegkapanadze@gmail.com>
2025-02-03 14:21:11 +0400

    refactor(react): reworked the textcard component

--- packages/react/src/components/Cards/TextCard/TextCard.tsx
+++ packages/react/src/components/Cards/TextCard/TextCard.tsx
@@ -1,69 +1,117 @@
-import { FC } from "react";
-import classnames from "classnames";
+import classNames from "classnames";
+import { forwardRef } from "react";
+
 import useGlobalSettings from "../../../hooks/useGlobalSettings";
-import { TextCardProps } from "./TextCard.props";
-import { Profile } from "../../Profile";
+import { Profile, ProfileProps } from "../../Profile";
+import { CardSize, EventDate, HeadingTypes, ThemeTypes } from "../../../types";
 
-const TextCard: FC<TextCardProps> = ({
-  eyebrow,
-  title,
-  theme,
-  size,
-  date,
-  profile,
-  link,
-  titleLevel: TitleElement,
-}) => {
-  const { prefix } = useGlobalSettings();
+export type TextCardProps = {
+  /**
+   * A line of text that appears as a small heading above the title of the card
+   */
+  eyebrow?: string;
 
-  const baseClass = `${prefix}--card`;
+  /**
+   * The title of the card
+   */
+  title: string;
 
-  const wrapperClass = classnames(`${baseClass}--wrapper`);
+  /**
+   * HTML element used for the title
+   */
+  titleLevel?: HeadingTypes;
 
-  const cardClasses = classnames(baseClass, `${baseClass}__type__text`, {
-    [`${baseClass}__action`]: link,
-    [`${baseClass}__size__${size as string}`]: size,
-    [`${baseClass}__theme__${theme}`]: theme,
-  });
+  /**
+   * Will render the card to appear on light or dark backgrounds
+   */
+  theme?: ThemeTypes;
 
-  return (
-    <div className={wrapperClass}>
-      <div className={cardClasses}>
-        {link && (
+  /**
+   * How big should the card be
+   */
+  size?: Omit<CardSize, "standard">;
+
+  /**
+   * Specify the event Date, in both human and Unix format.
+   */
+  date?: EventDate;
+
+  /**
+   * Profile information to be displayed on the card
+   */
+  profile: ProfileProps;
+
+  /**
+   * Specify the URL for the card link
+   */
+  link?: string;
+
+  /**
+   * Specify an optional className to be added to your TextCard.
+   */
+  className?: string;
+};
+
+const TextCard = forwardRef<HTMLDivElement, TextCardProps>(
+  (
+    {
+      className,
+      title,
+      theme,
+      size = "narrow",
+      date,
+      profile,
+      link,
+      titleLevel: TitleElement = "p",
+      eyebrow,
+    },
+    ref
+  ) => {
+    const { prefix } = useGlobalSettings();
+
+    const baseClass = `${prefix}--card`;
+
+    const wrapperClass = classNames(`${baseClass}--wrapper`, className);
+
+    const cardClasses = classNames(baseClass, `${baseClass}__type__text`, {
+      [`${baseClass}__action`]: link,
+      [`${baseClass}__size__${String(size)}`]: size,
+      [`${baseClass}__theme__${theme}`]: theme,
+    });
+
+    return (
+      <div className={wrapperClass} ref={ref}>
+        <div className={cardClasses}>
           <a className={`${baseClass}--link`} href={link} title={title}>
             <span className={`${baseClass}--link--text`}>{title}</span>
           </a>
-        )}
-        <div className={`${baseClass}--wrap`}>
-          <div className={`${baseClass}--content`}>
-            {eyebrow && <p className={`${baseClass}--eyebrow`}>{eyebrow}</p>}
-            {title && TitleElement ? (
+          <div className={`${baseClass}--wrap`}>
+            <div className={`${baseClass}--content`}>
+              {eyebrow && <p className={`${baseClass}--eyebrow`}>{eyebrow}</p>}
               <TitleElement className={`${baseClass}--title`}>
                 {title}
               </TitleElement>
-            ) : (
-              <p className={`${baseClass}--title`}>{title}</p>
-            )}
-            {date && (
-              <time className={`${baseClass}--date`} dateTime={date.unix}>
-                {date.human}
-              </time>
-            )}
-            {profile && profile.avatar && (
-              <Profile
-                avatar={profile.avatar}
-                description={profile.description}
-                link={profile.link}
-                name={profile.name}
-                role={profile.role}
-                className={`${prefix}--profile__theme__${theme}`}
-              />
-            )}
+              {date && (
+                <time className={`${baseClass}--date`} dateTime={date.unix}>
+                  {date.human}
+                </time>
+              )}
+              {profile && (
+                <Profile
+                  avatar={profile.avatar}
+                  description={profile.description}
+                  link={profile.link}
+                  name={profile.name}
+                  role={profile.role}
+                  theme={theme}
+                />
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
-export default TextCard;
+export { TextCard };
--- packages/react/src/components/Cards/TextCard/index.tsx
+++ packages/react/src/components/Cards/TextCard/index.tsx
@@ -1,3 +1,1 @@
-export { default as TextCard } from "./TextCard";
-export type { TextCardProps } from "./TextCard.props";
-export { TextCardArgs } from "./TextCard.args";
+export * from "./TextCard";
