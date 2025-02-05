6e0b04be82ac9175f2b295756b01e98becdc791b
GGKapanadze <doublegkapanadze@gmail.com>
2025-02-05 18:13:21 +0400

    refactor(react): reworked the detail card component

--- packages/react/src/components/Cards/DetailCard/index.tsx
+++ packages/react/src/components/Cards/DetailCard/index.tsx
@@ -1,3 +1,1 @@
-export { default as DetailCard } from "./DetailCard";
-export type { DetailCardProps } from "./DetailCard.props";
-export { DetailCardArgs } from "./DetailCard.args";
+export * from "./DetailCard";
