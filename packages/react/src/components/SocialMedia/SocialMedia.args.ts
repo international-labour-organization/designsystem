758dcc8db817914b268223e41a0902095350bd22
justintemps <jesmith627@gmail.com>
2025-02-06 16:08:39 +0100

    feat(react): socialmedia supports bluesky, weibo and wechat

---
+++ packages/react/src/components/SocialMedia/SocialMedia.args.ts
@@ -0,0 +1,60 @@
+import { SocialMediaProps } from "./SocialMedia.props";
+
+export const socialMediaArgs: SocialMediaProps = {
+  headline: "Follow us on social media",
+  theme: "light",
+  justify: "start",
+  iconSize: "normal",
+  icons: [
+    {
+      icon: "bluesky",
+      label: "Bluesky",
+      url: "https://bsky.app/profile/ilo.org",
+    },
+    {
+      icon: "facebook",
+      url: "https://www.facebook.com/ilo.org",
+      label: "Facebook",
+    },
+    {
+      icon: "x",
+      url: "https://x.com/ilo",
+      label: "x",
+    },
+    {
+      icon: "instagram",
+      url: "https://www.instagram.com/ilo.org/",
+      label: "Instagram",
+    },
+    {
+      icon: "linkedin",
+      url: "https://www.linkedin.com/company/international-labour-organization-ilo",
+      label: "LinkedIn",
+    },
+    {
+      icon: "youtube",
+      url: "https://www.youtube.com/channel/UCQsVmhSa4X-G3lHlUtejzLA",
+      label: "YouTube",
+    },
+    {
+      icon: "tiktok",
+      url: "https://www.tiktok.com/@ilo",
+      label: "TikTok",
+    },
+    {
+      icon: "flickr",
+      url: "https://www.flickr.com/photos/ilopictures/albums",
+      label: "flickr",
+    },
+    {
+      icon: "wechat",
+      label: "Wechat",
+      url: "https://wechat.com",
+    },
+    {
+      icon: "weibo",
+      label: "Weibo",
+      url: "https://weibo.com",
+    },
+  ],
+};
