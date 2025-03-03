525322e47b02294ad584b5e89bec21d8d9714f94
justintemps <jesmith627@gmail.com>
2025-03-03 23:23:29 +0100

    feat(twig/styles): Score Card

---
+++ packages/twig/stories/card_score.stories.js
@@ -0,0 +1,16 @@
+import CardEvent from "../src/components/card_score/card_score.twig";
+import CardEventPatterns from "../src/components/card_score/card_score.component.yml";
+import { Maestro } from "@ilo-org/maestro";
+
+const story = Maestro.create(CardEvent, CardEventPatterns);
+
+const Meta = {
+  title: "Components/Cards/Score Card",
+  tags: ["autodocs"],
+  ...story.meta,
+};
+
+const [Default] = story.stories;
+
+export default Meta;
+export { Default };
