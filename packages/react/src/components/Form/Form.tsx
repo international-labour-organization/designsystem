69426f664f13768c66ac85c192ab150f5c858c56
justintemps <jesmith627@gmail.com>
2025-05-18 16:09:31 +0200

    fix(react): correctly inherit form color

--- packages/react/src/components/Form/Form.tsx
+++ packages/react/src/components/Form/Form.tsx
@@ -4,23 +4,21 @@ import classNames from "classnames";
 import { FormProps } from "./Form.props";
 
 import { createContext, useContext } from "react";
+import { ThemeTypes } from "../../types";
 
 interface FormContextValue {
-  theme: "light" | "dark";
+  theme: ThemeTypes | null;
 }
 
-const FormContext = createContext<FormContextValue>({ theme: "light" });
+const FormContext = createContext<FormContextValue>({ theme: null });
 
 export const useFormContext = () => {
   const context = useContext(FormContext);
-  if (!context) {
-    throw new Error("useFormContext must be used within a Form component");
-  }
   return context;
 };
 
 const Form = forwardRef<HTMLFormElement, FormProps>(
-  ({ children, theme = "light", ...props }, ref) => {
+  ({ children, theme = null, ...props }, ref) => {
     const { prefix } = useGlobalSettings();
 
     const baseClass = `${prefix}--form`;
