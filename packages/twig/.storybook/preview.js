85599c276917bdd07d12c5068bce6ecaf1d83666
justintemps <jesmith627@gmail.com>
2025-06-19 16:23:45 +0200

    BREAKING CHANGE: Arabic font changed from Noto Sans Arabic to Vazirmatn

--- packages/twig/.storybook/preview.js
+++ packages/twig/.storybook/preview.js
@@ -8,7 +8,53 @@ import "./styles.scss";
 
 /** @type { import('@storybook/html').Preview } */
 const preview = {
-  decorators: [BehaviorDecorator],
+  globalTypes: {
+    locale: {
+      description: "Locale",
+      toolbar: {
+        icon: "globe",
+        items: [
+          { value: "en", title: "English" },
+          { value: "ar", title: "Arabic" },
+          { value: "zh", title: "Chinese" },
+          { value: "ja", title: "Japanese" },
+          { value: "ru", title: "Russian" },
+        ],
+      },
+    },
+    dir: {
+      description: "Direction",
+      toolbar: {
+        icon: "transfer",
+        items: [
+          { value: "ltr", title: "LTR" },
+          { value: "rtl", title: "RTL" },
+        ],
+      },
+    },
+  },
+  initialGlobals: {
+    locale: "en",
+    dir: "ltr",
+  },
+  decorators: [
+    BehaviorDecorator,
+    (Story, context) => {
+      const wrapper = document.createElement("div");
+
+      if (context.globals.locale) {
+        wrapper.lang = context.globals.locale;
+      }
+
+      if (context.globals.dir) {
+        wrapper.dir = context.globals.dir;
+      }
+
+      wrapper.appendChild(Story());
+
+      return wrapper;
+    },
+  ],
   parameters: {
     options: {
       storySort: {
