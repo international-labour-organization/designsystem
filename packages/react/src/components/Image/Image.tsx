532fdeb3f70e9cb9da18b8c73c8401579c0f4606
Prabashwara Seneviratne <contact@bash.lk>
2025-09-22 18:50:03 +0200

    fix(styles): ported changes to react component (#1543)

--- packages/react/src/components/Image/Image.tsx
+++ packages/react/src/components/Image/Image.tsx
@@ -4,17 +4,25 @@ import useGlobalSettings from "../../hooks/useGlobalSettings";
 import { ImageProps, ImageUrl } from "./Image.props";
 import { Tooltip } from "../Tooltip";
 
-const Image: FC<ImageProps> = ({ alt, caption, className, credit, url }) => {
+const Image: FC<ImageProps> = ({
+  alt,
+  caption,
+  className,
+  credit,
+  url,
+  theme,
+}) => {
   const { prefix } = useGlobalSettings();
   const baseClass = `${prefix}--image`;
 
   const imageClasses = classNames(className, {
     [baseClass]: true,
+    [`${baseClass}__theme__${theme}`]: theme,
   });
 
   return (
     <figure className={imageClasses}>
-      <div className={`${imageClasses}--wrapper`}>
+      <div className={`${baseClass}--wrapper`}>
         <picture>
           {url &&
             url
@@ -31,8 +39,14 @@ const Image: FC<ImageProps> = ({ alt, caption, className, credit, url }) => {
           {url && <img src={url[0].src} alt={alt} />}
         </picture>
         {credit && (
-          <div className={`${imageClasses}--credit`}>
-            <Tooltip label={credit} iconTheme="dark" />
+          <div className={`${baseClass}--credit`}>
+            <div className={`${baseClass}--tooltip`}>
+              <Tooltip
+                label={credit}
+                iconTheme={theme === "dark" ? "light" : "dark"}
+              />
+            </div>
+            <div className={`${baseClass}--label`}>{credit}</div>
           </div>
         )}
       </div>
