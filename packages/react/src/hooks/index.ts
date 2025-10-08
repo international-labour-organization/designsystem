c21520619c2c612adde9f50fac5f6d6719b063ba
GGKapanadze <doublegkapanadze@gmail.com>
2025-10-08 20:06:59 +0400

    feat(react): implemented the foucs trap hook

--- packages/react/src/hooks/index.ts
+++ packages/react/src/hooks/index.ts
@@ -2,3 +2,4 @@ export { default as useGlobalSettings } from "./useGlobalSettings";
 export { default as usePrevious } from "./usePrevious";
 export { default as useBreakpoint } from "./useBreakpoint";
 export { default as useOutsideClick } from "./useOutsideClick";
+export { default as useFocusTrap } from "./useFocusTrap";
