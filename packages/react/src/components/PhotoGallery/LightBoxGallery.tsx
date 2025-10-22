7934d70ebf89f50166cc27039d7349e27decdf0a
GGKapanadze <doublegkapanadze@gmail.com>
2025-10-22 17:54:20 +0400

    feat(react): implemented the initial light box gallery

---
+++ packages/react/src/components/PhotoGallery/LightBoxGallery.tsx
@@ -0,0 +1,137 @@
+import { useEffect, useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import classNames from "classnames";
+
+import { PhotoGalleryItem } from "./PhotoGallery";
+import { useGlobalSettings } from "../../hooks";
+import {
+  PhotoGalleryControls,
+  usePhotoGalleryControls,
+} from "./PhotoGalleryControls";
+
+interface LightBoxProps {
+  items: PhotoGalleryItem[];
+  initialIndex?: number;
+  onSelect?: (index: number) => void;
+}
+
+function LightBoxGallery({
+  items,
+  initialIndex = 0,
+  onSelect: parentOnSelect,
+}: LightBoxProps) {
+  const { prefix } = useGlobalSettings();
+  const [currentIndex, setCurrentIndex] = useState(initialIndex);
+  const [emblaRef, emblaAPI] = useEmblaCarousel({
+    loop: true,
+    startIndex: initialIndex,
+    direction: document.dir === "rtl" ? "rtl" : "ltr",
+    duration: 30,
+  });
+  const [thumbRef, thumbAPI] = useEmblaCarousel({
+    containScroll: "keepSnaps",
+    direction: document.dir === "rtl" ? "rtl" : "ltr",
+    dragFree: true,
+    duration: 30,
+  });
+  const controls = usePhotoGalleryControls(emblaAPI);
+
+  const baseClass = `${prefix}--lightbox-gallery`;
+  const coreClass = `${baseClass}__core`;
+  const thumbnailClass = `${baseClass}__thumbnails`;
+
+  const onSelect = () => {
+    if (!emblaAPI || !thumbAPI) return;
+    setCurrentIndex(emblaAPI.selectedScrollSnap());
+    parentOnSelect?.(emblaAPI.selectedScrollSnap());
+    thumbAPI.scrollTo(emblaAPI.selectedScrollSnap());
+  };
+
+  const onThumbnailClick = (index: number) => {
+    if (!emblaAPI || !thumbAPI) return;
+    emblaAPI.scrollTo(index);
+  };
+
+  useEffect(() => {
+    if (!emblaAPI) return;
+
+    emblaAPI.on("select", onSelect);
+
+    return () => {
+      emblaAPI.off("select", onSelect);
+    };
+  }, [emblaAPI]);
+
+  return (
+    <div className={baseClass}>
+      <div className={coreClass}>
+        <div className={`${coreClass}__viewport`} ref={emblaRef}>
+          <div className={`${coreClass}__container`}>
+            {items.map((item, index) => (
+              <div className={`${coreClass}__slide`} key={index}>
+                <figure className={`${coreClass}__image-wrapper`}>
+                  <img
+                    src={item.url as string}
+                    alt={item.credit || `Photo ${index + 1}`}
+                  />
+                  {item.credit && (
+                    <figcaption
+                      className={`${coreClass}__image-wrapper-credit`}
+                    >
+                      <span
+                        className={`${coreClass}__image-wrapper-credit-text`}
+                      >
+                        {item.credit}
+                      </span>
+                      <span
+                        className={`${coreClass}__image-wrapper-credit-corner`}
+                      ></span>
+                    </figcaption>
+                  )}
+                </figure>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className={`${baseClass}__bar`}>
+        <div className={`${baseClass}__extra`}>
+          <caption className={`${baseClass}__extra-text`}>
+            {items[currentIndex]?.caption}
+          </caption>
+          <PhotoGalleryControls
+            onNext={controls.onNextButtonClick}
+            onPrev={controls.onPrevButtonClick}
+            onFirst={controls.onFirstButtonClick}
+            onLast={controls.onLastButtonClick}
+            indicator={{ current: currentIndex + 1, total: items.length }}
+          />
+        </div>
+        <div className={thumbnailClass}>
+          <div className={`${thumbnailClass}__carousel`} ref={thumbRef}>
+            <div className={`${thumbnailClass}__container`}>
+              {items.map((item, index) => (
+                <div className={`${thumbnailClass}__slide`} key={index}>
+                  <button
+                    className={classNames(`${thumbnailClass}__image`, {
+                      [`${thumbnailClass}__image--selected`]:
+                        index === currentIndex,
+                    })}
+                    onClick={() => onThumbnailClick(index)}
+                  >
+                    <img
+                      src={item.url as string}
+                      alt={item.credit || `Thumbnail ${index + 1}`}
+                    />
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { LightBoxGallery };
